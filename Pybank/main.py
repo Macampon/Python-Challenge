9c2bf8940ac2b37481b465397ff06873a47d6561
Macampon <gianna_abono@yahoo.com>
2021-08-25 02:04:32 +1000

    new copy

--- Pybank/main.py
+++ Pybank/main.py
@@ -17,12 +17,12 @@ with open(csvpath,newline='') as budget:
     amount=columns['Profit/Losses']
     max_profit=0.0
     max_loss=0.0
-    ctotal=0.0
-    gtotal=int(amount[0])
+    ctotal=0.0                #change total per month
+    gtotal=int(amount[0])     #total profit/loss 
     ptotal=amount[0]
     average=0.0
     ctr=1
-    mylist=[]
+    mylist=[]                  #profit/loss changes list
     max_idx=0
     min_idx=0
     pmlist=0
@@ -45,9 +45,9 @@ with open(csvpath,newline='') as budget:
           min_idx = c+1
           #print(min_idx)
     
-    for a, b in enumerate(mylist):
-       if b==max(mylist):
-          max_idx=a+1
+    
+       if d==max(mylist):
+          max_idx=c+1
          # print(max_idx)
         
     tot_diff=sum(mylist)
@@ -68,3 +68,19 @@ with open(csvpath,newline='') as budget:
     print("Greatest Decrease in Profit :  " + month[min_idx] + "  ($"  + str(min_profit)+")")
 
    # print("check profit/loss Jan amount   " + str(ptotal))
+
+# Specify the file to write to
+output_path = os.path.join("..", "Resources", "analysis_paybank.txt")
+
+with open(output_path,'w', newline='') as file:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(file)
+    csvwriter.writerow(["Financial Analysis"])
+    csvwriter.writerow(["----------------------------"])
+    csvwriter.writerow(" ")
+    csvwriter.writerow(["Total Months : "+str(ctr)])
+    csvwriter.writerow(["Total Profit/Loss : "+str({gtotal})])
+    csvwriter.writerow(["Average Profit/Loss : " +str({average})])
+    csvwriter.writerow(["Greatest Increase in Profit  :  "  + str(month[max_idx]) + "  ($"  + str(max_profit)+")"])
+    csvwriter.writerow(["Greatest Decrease in Profit :  " + str(month[min_idx]) + "  ($"  + str(min_profit)+")"])
